2174203073b23dc982270678019d935a760598a3
ittkkkiitt <88095712+ittkkkiitt@users.noreply.github.com>
2022-11-22 06:46:44 +0300

    Delete shop directory

--- shop/js/swiper-fragnance.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl:  '.swiper-button-prev'
-            }
-        });
-
-    
