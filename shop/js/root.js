d1b91b8e38441dd905539576f4d25c66b56538c4
Polina <88095712+ittkkkiitt@users.noreply.github.com>
2022-11-24 01:56:11 +0300

    Delete shop/js directory

--- shop/js/root.js
+++
@@ -1,4 +0,0 @@
-const ROOT_PRODUCTS = document.getElementById('container');
-const ROOT_SHOP = document.getElementById('shopping-cart');
-const  ROOT_HEADER = document.getElementById('header-content');
-
